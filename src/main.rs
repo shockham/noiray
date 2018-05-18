16026ea4139b3fbf3153b271c191a7507fbc8e49
shockham <samuel.hockham@gmail.com>
2018-05-18 11:19:04 +0100

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+extern crate caper;
+
+use caper::game::*;
+use caper::imgui::Ui;
+use caper::input::Key;
+use caper::types::DefaultTag;
+use caper::utils::handle_fp_inputs;
+use caper::shader;
+
+use std::str;
+
+fn add_frag(game: &mut Game<DefaultTag>, name: &'static str, shader: &'static str) {
+    game.renderer
+        .shaders
+        .add_post_shader(
+            &game.renderer.display,
+            name,
+            shader::post::gl330::VERT,
+            shader,
+        )
+        .unwrap();
+
+}
+
+fn main() {
+    // crate an instance of the game struct
+    let mut game = Game::<DefaultTag>::new();
+
+    {
+        add_frag(&mut game, "scene1", str::from_utf8(include_bytes!("frag.glsl")).unwrap());
+    }
+
+    game.renderer.post_effect.current_shader = "scene1";
+
+    loop {
+        // run the engine update
+        let status = game.update(
+            |_: &Ui| {},
+            |g: &mut Game<DefaultTag>| -> UpdateStatus {
+                // update the first person inputs
+                handle_fp_inputs(&mut g.input, &mut g.cams[0]);
+
+                // quit
+                if g.input.keys_down.contains(&Key::Escape) {
+                    return UpdateStatus::Finish;
+                }
+
+                UpdateStatus::Continue
+            },
+        );
+
+        if let UpdateStatus::Finish = status {
+            break;
+        }
+    }
+}
